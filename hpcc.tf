211f2bf6b6b6af1db5b8bfcdf011474af4b7b6bc
Godji Fortil <godson.fortil@lexisnexisrisk.com>
2023-08-25 10:22:22 -0400

    Output ECL Watch domain and add remote source for the HPCC module
    Signed-off-by: Godji Fortil <godson.fortil@lexisnexisrisk.com>

--- hpcc.tf
+++ hpcc.tf
@@ -9,8 +9,8 @@ resource "kubernetes_namespace" "hpcc" {
 }
 
 module "hpcc" {
-  # source = "github.com/gfortil/opinionated-terraform-azurerm-hpcc?ref=HPCC-27615"
-  source = "../../opinionated/opinionated-terraform-azurerm-hpcc"
+  source = "github.com/gfortil/opinionated-terraform-azurerm-hpcc?ref=HPCC-27615"
+  # source = "../../opinionated/opinionated-terraform-azurerm-hpcc"
 
   environment = var.metadata.environment
   productname = var.metadata.product_name
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "hpcc_namespace" {
   description = "The namespace where the HPCC Platform is deployed."
   value       = local.hpcc_namespace
 }
+
+output "eclwatch" {
+  description = "Print the ECL Watch domain out."
+  value       = local.svc_domains.eclwatch
+}
