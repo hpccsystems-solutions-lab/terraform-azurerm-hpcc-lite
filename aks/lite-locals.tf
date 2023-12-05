0274223deb6ea0063c3cec16ea0034adf29360aa
Ubuntu <azureuser@tlh-dev-linux-vm2.pkn05rdko4jejloub4w2s5lh3e.cx.internal.cloudapp.net>
2023-12-05 21:12:54 +0000

    Removed metadata from aks, hpcc, and vnet (not in storage). Now it is copied from root directory to each of these directories.

--- aks/lite-locals.tf
+++ aks/lite-locals.tf
@@ -10,19 +10,6 @@ locals {
 
   owner_name_initials = lower(join("",[for x in split(" ",local.owner.name): substr(x,0,1)]))
 
-  metadata = {
-    project             = format("%shpccplatform", local.owner_name_initials)
-    product_name        = format("%shpccplatform", local.owner_name_initials)
-    business_unit       = "commercial"
-    environment         = "sandbox"
-    market              = "us"
-    product_group        = format("%shpcc", local.owner_name_initials)
-    resource_group_type = "app"
-    sre_team            = format("%shpccplatform", local.owner_name_initials)
-    subscription_type   = "dev"
-    location            = var.aks_azure_region # Acceptable values: eastus, centralus
-  }
-
   core_services_config = {
     alertmanager = {
       smtp_host = "smtp-hostname.ds:25"
---
+++ metadata.tf
@@ -0,0 +1,14 @@
+locals {
+  metadata = {
+    project             = format("%shpccplatform", local.owner_name_initials)
+    product_name        = format("%shpccplatform", local.owner_name_initials)
+    business_unit       = "commercial"
+    environment         = "sandbox"
+    market              = "us"
+    product_group        = format("%shpcc", local.owner_name_initials)
+    resource_group_type = "app"
+    sre_team            = format("%shpccplatform", local.owner_name_initials)
+    subscription_type   = "dev"
+    location            = var.aks_azure_region # Acceptable values: eastus, centralus
+  }
+}
